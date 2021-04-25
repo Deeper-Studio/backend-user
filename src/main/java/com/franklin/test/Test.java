ef5d4d41edff818227c92ad497ff882b4ebfb4ce
Thomas-muller-25 <18726077@bjtu.edu.cn>
2021-04-25 17:12:28 +0800

    Delete test

--- src/main/java/com/franklin/test/Test.java
+++ src/main/java/com/franklin/test/Test.java
@@ -1,12 +1,4 @@
 package com.franklin.test;
 
 public class Test {
-    public static void main (String[] args) {
-        Object object = new Object();
-        object = "admin,organization,staff";
-        String[] perms = (String[]) object;
-        for(String permStr : perms){
-            System.out.println(permStr);
-        }
-    }
 }
